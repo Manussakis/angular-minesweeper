132ff5c8cb7479a5c03e0cd6247be178eddac966
Gabriel Manussakis <gabriel.manussakis@envision.com.br>
2020-03-29 14:53:31 -0300

    Hammer config - touchAction auto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CellComponent } from './cell/cell.component';
 import { StopwatchPipe } from './pipes/stopwatch.pipe';
 import { PressEventIndicatorDirective } from './directives/press-event-indicator.directive';
+import * as Hammer from 'hammerjs';
+
+@Injectable()
+export class MinesweeperHammerConfig extends HammerGestureConfig {
+    buildHammer(element: HTMLElement) {
+        const ta = new Hammer(element, {
+            touchAction: "auto",
+        });
+        return ta;
+    }
+}
 
 @NgModule({
     declarations: [
@@ -18,6 +29,10 @@ import { PressEventIndicatorDirective } from './directives/press-event-indicator
         FormsModule,
         HammerModule,
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    providers: [{
+        provide: HAMMER_GESTURE_CONFIG,
+        useClass: MinesweeperHammerConfig
+    }]
 })
 export class AppModule { }
