abead674b30468eed4c23aecff2983ec5bf439bd
Gabriel Manussakis <gabriel.manussakis@envision.com.br>
2020-05-09 17:01:55 -0300

    Remove unnecessary import

--- src/app/best-scores-table/best-scores-table.module.ts
+++ src/app/best-scores-table/best-scores-table.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BestScoresTableComponent } from './best-scores-table.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [BestScoresTableComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [BestScoresTableComponent]
+    declarations: [BestScoresTableComponent],
+    imports: [SharedModule],
+    exports: [BestScoresTableComponent]
 })
 export class BestScoresTableModule { }
