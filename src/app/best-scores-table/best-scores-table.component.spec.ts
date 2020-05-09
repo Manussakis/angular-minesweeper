85037277da3664304e1ed310834571a7eeaafbdb
Gabriel Manussakis <gabriel.manussakis@envision.com.br>
2020-05-09 17:02:33 -0300

    Test best scores table component

--- src/app/best-scores-table/best-scores-table.component.spec.ts
+++ src/app/best-scores-table/best-scores-table.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+
 import { BestScoresTableComponent } from './best-scores-table.component';
+import { ScoreService } from '../core/score.service';
+import { MockStopWatch } from '../app.component.spec';
 
 describe('BestScoresTableComponent', () => {
-  let component: BestScoresTableComponent;
-  let fixture: ComponentFixture<BestScoresTableComponent>;
+    let component: BestScoresTableComponent;
+    let fixture: ComponentFixture<BestScoresTableComponent>;
+    let mockScoreService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BestScoresTableComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        mockScoreService = {
+            bestScores$: of({ easy: 35, medium: 100, hard: 200 })
+        };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BestScoresTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.configureTestingModule({
+            declarations: [BestScoresTableComponent, MockStopWatch],
+            providers: [
+                { provide: ScoreService, useValue: mockScoreService }
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BestScoresTableComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should assign correct value to bestScoresKeys property', () => {
+        expect(component.bestScoresKeys).toEqual(['easy', 'medium', 'hard']);
+    });
 });
