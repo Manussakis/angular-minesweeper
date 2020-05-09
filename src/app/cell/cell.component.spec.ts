4dfa9dd527a9fa19a43621ed90f5a04d2f172cb1
Gabriel Manussakis <gabriel.manussakis@envision.com.br>
2020-05-09 17:02:52 -0300

    Test cell component

--- src/app/cell/cell.component.spec.ts
+++ src/app/cell/cell.component.spec.ts
@@ -1,25 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { CellComponent } from './cell.component';
+import { MinesweeperService } from '../core/minesweeper.service';
+import { By } from '@angular/platform-browser';
 
 describe('CellComponent', () => {
-  let component: CellComponent;
-  let fixture: ComponentFixture<CellComponent>;
+    let component: CellComponent;
+    let fixture: ComponentFixture<CellComponent>;
+    let mockMinesweeperService;
+    const cellMock = {
+        i: 0,
+        isMine: false,
+        isMineExploded: false,
+        isOpened: true,
+        label: "1",
+        type: 1,
+        x: 0,
+        y: 0,
+        openedIdClassName: 'opened-1',
+    };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CellComponent ]
+    beforeEach(async(() => {
+        mockMinesweeperService = jasmine.createSpyObj(['']);
+
+        TestBed.configureTestingModule({
+            declarations: [CellComponent],
+            providers: [
+                { provide: MinesweeperService, useValue: mockMinesweeperService }
+            ]
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CellComponent);
+        component = fixture.componentInstance;
+        component.cell = cellMock;
+        fixture.detectChanges();
     })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CellComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should render correct className', () => {        
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.className).toContain("cell");
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.className).toContain("opened-1");
+        expect(fixture.debugElement.query(By.css('button')).nativeElement.className).toContain("opened");
+    });
 });
