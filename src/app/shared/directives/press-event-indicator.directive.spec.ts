8afa895bc45e6d658f86172b30057cb35954f60f
Gabriel Manussakis <gabriel.manussakis@envision.com.br>
2020-05-09 17:05:10 -0300

    Comment test as TODO

--- src/app/shared/directives/press-event-indicator.directive.spec.ts
+++ src/app/shared/directives/press-event-indicator.directive.spec.ts
@@ -1,8 +1,1 @@
-// import { PressEventIndicatorDirective } from './press-event-indicator.directive';
-
-// describe('PressEventIndicatorDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new PressEventIndicatorDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+// TODO: Test directive
